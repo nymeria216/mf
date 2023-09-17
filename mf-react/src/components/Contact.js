afe2618b37ee18b38bf72b9eaacd8a8e142eb37b
nymeria216 <melissa.futtig@protonmail.com>
2023-09-17 23:11:28 +0200

    news

--- mf-react/src/components/Contact.js
+++ mf-react/src/components/Contact.js
@@ -1,32 +1,4 @@
-import React, { useState }from "react";
-
-// Stile für die Webseite (du kannst sie anpassen)
-const styles = {
-  container: {
-    maxWidth: "800px",
-    margin: "0 auto",
-    padding: "20px",
-  },
-  section: {
-    marginBottom: "20px",
-  },
-  passwordInput: {
-    marginRight: "10px",
-  },
-  button: {
-    padding: "10px 20px",
-    background: "#007bff",
-    color: "#fff",
-    border: "none",
-    cursor: "pointer",
-  },
-  footer: {
-    backgroundColor: "#333",
-    color: "#fff",
-    padding: "10px 0",
-    textAlign: "center",
-  },
-};
+import React from "react";
 
 const Contact = () => {
 
